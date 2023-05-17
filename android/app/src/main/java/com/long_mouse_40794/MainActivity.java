b20a91a3e0a906d40008345615c828b1ffb1e2e2
Crowdbotics <team@crowdbotics.com>
2023-05-17 20:27:03 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/long_mouse_40794/MainActivity.java
@@ -0,0 +1,15 @@
+package com.long_mouse_40794;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "long_mouse_40794";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "long-mouse-40794",
+  url: "https://long-mouse-40794.botics.co",
+  api: "https://long-mouse-40794.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
